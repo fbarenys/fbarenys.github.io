bc6696d4982ded84dde4284e8283ae025dbceeab
Felix <fbarenys.fb@gmail.com>
2020-08-20 13:32:26 +0200

    First commit
    1st web

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+document.getElementById("age").innerHTML = calculate_age(new Date(1998, 10, 19));
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
